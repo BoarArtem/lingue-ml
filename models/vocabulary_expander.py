48a68ca007a3745fe53d20681e04b5356940aaa2
unknown <workboar@gmail.com>
2026-02-19 19:54:39 +0200

    Полностью подтвержден метод для модели, были исправлены ошибки и добавлена типизация вместе с описанием модели

--- models/vocabulary_expander.py
+++ models/vocabulary_expander.py
@@ -2,9 +2,13 @@ from gensim.models import KeyedVectors
 
 wv = KeyedVectors.load("../inference/google_news.kv", mmap="r")
 
-def pretrained_model(arr):
+def vocabulary_expander(arr: list) -> list:
+    """
+    Модель будет смотреть на список пользователя и подбирать максимально те слова которые больше всего подходят для этого списка
+    :param arr: Список со словами пользователя. Иначе - колода пользователя
+    :return: Возвращает нам метод (positive=arr) - список пользователя по которому модель будет подбирать слова, (topn=10) - топ-10 слов которые имеют высший процент схожести
+    """
     return wv.most_similar(
-        positive=arr
+        positive=arr,
+        topn=10
     )
-
-print(pretrained_model(["banana", "apple", "mango"]))
