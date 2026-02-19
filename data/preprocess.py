37ddcbf793f006b05b9faeef3e4359aa0d1bc09a
unknown <workboar@gmail.com>
2026-02-19 18:10:43 +0200

    Добавлена модель для дополнения слов из имеющиехся. Был использован метод в котором взята уже готовая модель

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,15 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def vocabulary_expander_preprocess():
-    with open("./datasets/words-en.txt", "r", encoding="utf-8") as f:
-        words = [line.strip().lower() for line in f if line.strip()]
-        words = [w for w in words if len(w) >= 3]
-
-        words =  list(set(words))
-
-    return words
-
 def b2_time_prediction_preprocess(filepath):
     df = pd.read_csv(filepath)
     target_col = 'target_days_b2'
