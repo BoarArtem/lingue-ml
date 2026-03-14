47501322e878df4803f860e517dfac261cfcd200
BoarArtem <workboar@gmail.com>
2026-03-15 01:03:21 +0200

    Remove print statement for model similarities
    Removed print statement for most similar words.

--- inference/ve_model.py
+++ inference/ve_model.py
@@ -3,5 +3,3 @@ from gensim.models import Word2Vec
 model_path = "word2vec.model"
 
 model = Word2Vec.load(model_path)
-
-print(model.wv.most_similar(["pizza", "sushi", "coffee", "chocolate", "bread"], topn=10))
