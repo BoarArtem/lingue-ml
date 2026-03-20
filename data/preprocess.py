0bb9492fcffff4f86e93268da39fe5d9f9ebaa38
unknown <workboar@gmail.com>
2026-03-20 23:41:13 +0200

    Очищен датасет и так же проведены были некоторые манипуляции с токенизацией.. вылезла ошибка и вылез мой геморой!

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 def b2_time_prediction_preprocess(filepath):
     df = pd.read_csv(filepath)
@@ -9,3 +10,10 @@ def b2_time_prediction_preprocess(filepath):
     y = df[target_col]
 
     return train_test_split(X, y, test_size=0.2, random_state=42)
+
+def spam_classification_preprocess(filepath):
+    data = pd.read_csv(filepath)
+
+    data['label'] = data['label'].map({'Spam': 1, 'Ham': 0}) # encoding label data like 0 and 1, where 1 - spam, 0 - ham
+
+    return data
