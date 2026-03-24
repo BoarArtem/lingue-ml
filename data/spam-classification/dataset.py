e90d0ab88e62bfb2ca9e0e5024bbb74f2693f978
unknown <workboar@gmail.com>
2026-03-25 00:29:28 +0200

    Полностью дописан пайплан датасета. Немного исправил говнеца и проверил на трех типах тестов! Всё работает чётенька. Осталось сделать модель

--- data/spam-classification/dataset.py
+++ data/spam-classification/dataset.py
@@ -1,45 +1,33 @@
 import pickle
 from collections import Counter
-import torch
+import torch.nn
+from torch.nn.functional import embedding
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset, DataLoader
+
 from data.preprocess import spam_classification_preprocess
 
 data = spam_classification_preprocess("../datasets/spam_Emails_data.csv")
 
 X = data['text']
-y = data['labels']
-
+y = data['label']
 
 with open("../tokens_cache.pkl", "rb") as f:
     tokens = pickle.load(f)
 
 all_tokens = [w for text in tokens for w in text]
 
+# counter of token items
 counter = Counter(all_tokens)
 
+# vocab of tokens
 vocab = {
     w: i+1
     for i, (w, _) in enumerate(counter.most_common(10000))
 }
 
 
-# encoded text
-encoded_text = [
-    torch.tensor([vocab.get(w, 0) for w in text])
-    for text in tokens
-]
-
-# pad sequence
-padded = pad_sequence(encoded_text, batch_first=True, padding_value=0)
-
-# embedding
-embedding = torch.nn.Embedding(
-    num_embeddings=len(vocab) + 1,
-    embedding_dim=128,
-    padding_idx=0
-)
-
+# main dataset class
 class SpamDataset(Dataset):
     def __init__(self, vocab, tokens, labels, max_len=None):
         self.vocab = vocab
@@ -60,14 +48,14 @@ class SpamDataset(Dataset):
         if self.max_len:
             seq = seq[:self.max_len]
 
-        label = torch.tensor(self.labels[idx])
+        labels = torch.tensor(self.labels[idx])
 
-        return seq, label
+        return seq, labels
 
 def collate_fn(batch):
     sequence, labels = zip(*batch)
 
-    padded_sequences = pad_sequence(
+    padded_sequence = pad_sequence(
         sequence,
         batch_first=True,
         padding_value=0
@@ -75,7 +63,38 @@ def collate_fn(batch):
 
     labels = torch.tensor(labels)
 
-    return padded_sequences, labels
+    return padded_sequence, labels
 
 
-dataset = SpamDataset(vocab, tokens, y, max_len=128)
+# creating dataset object
+dataset = SpamDataset(vocab, tokens, y, 128)
+
+
+# creating dataloader
+loader = DataLoader(
+    dataset,
+    batch_size=32,
+    shuffle=True,
+    collate_fn=collate_fn
+)
+
+# main testing (1)
+# for X, y in loader:
+#     print(X.shape)
+#     print(y.shape)
+#     break
+
+# (2)
+# print(X[0][:10])
+# print(y[0])
+
+# embedding testing
+# embedding = torch.nn.Embedding(
+#     num_embeddings=len(vocab) + 1,
+#     embedding_dim=128,
+#     padding_idx=0
+# )
+#
+# emb = embedding(X)
+# print(emb)
+
