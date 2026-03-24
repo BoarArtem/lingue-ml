4cdba839e2494ad57aa18721826141ea33426f33
unknown <workboar@gmail.com>
2026-03-24 18:56:13 +0200

    Подправлены не большие детали перед классом, и написан сам класс датасета + создан экземпляр класса

--- data/spam-classification/dataset.py
+++ data/spam-classification/dataset.py
@@ -2,6 +2,14 @@ import pickle
 from collections import Counter
 import torch
 from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import Dataset, DataLoader
+from data.preprocess import spam_classification_preprocess
+
+data = spam_classification_preprocess("../datasets/spam_Emails_data.csv")
+
+X = data['text']
+y = data['labels']
+
 
 with open("../tokens_cache.pkl", "rb") as f:
     tokens = pickle.load(f)
@@ -32,3 +40,42 @@ embedding = torch.nn.Embedding(
     padding_idx=0
 )
 
+class SpamDataset(Dataset):
+    def __init__(self, vocab, tokens, labels, max_len=None):
+        self.vocab = vocab
+        self.labels = labels
+        self.max_len = max_len
+
+        self.encoded = [
+            torch.tensor([vocab.get(w, 0) for w in text])
+            for text in tokens
+        ]
+
+    def __len__(self):
+        return len(self.encoded)
+
+    def __getitem__(self, idx):
+        seq = self.encoded[idx]
+
+        if self.max_len:
+            seq = seq[:self.max_len]
+
+        label = torch.tensor(self.labels[idx])
+
+        return seq, label
+
+def collate_fn(batch):
+    sequence, labels = zip(*batch)
+
+    padded_sequences = pad_sequence(
+        sequence,
+        batch_first=True,
+        padding_value=0
+    )
+
+    labels = torch.tensor(labels)
+
+    return padded_sequences, labels
+
+
+dataset = SpamDataset(vocab, tokens, y, max_len=128)
