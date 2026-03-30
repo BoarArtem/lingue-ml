978827863cb631bb2b06d3089a8b4de7fb23fead
unknown <workboar@gmail.com>
2026-03-30 21:31:03 +0300

    Исправлены все проблемы, добавлена функция трейнинга модели

--- models/spam_classification_model.py
+++ models/spam_classification_model.py
@@ -39,12 +39,9 @@ criterion = nn.CrossEntropyLoss()
 # optimizers
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-# num of epochs
-epochs = 5
-
 model.train()
 
-if __name__ == "__main__":
+def train(epochs):
     for epoch in range(epochs):
         total_loss = 0
 
@@ -61,6 +58,10 @@ if __name__ == "__main__":
 
             total_loss += loss.item()
 
-        print(f"Epoch {epoch + 1}, Loss: {total_loss:.4f}")
+        print(f"Epoch {epoch+1}, Loss: {total_loss:.4f}")
 
-        torch.save(model.state_dict(), '../inference/spam_classification_model.pth')
+    # save model
+    torch.save(model.state_dict(), "../inference/spam_classification_model.pth")
+
+if __name__ == "__main__":
+    train(5)
