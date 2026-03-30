732ad0335c4b8c295918eabe4215bd4a60200c7e
Ruslan <120751851+ruslanpodolian@users.noreply.github.com>
2026-03-30 08:45:11 +0300

    Applied Changes in llm_word_level.py and llm_sentence_generate.py regarding environment variables

--- models/llm_word_level.py
+++ models/llm_word_level.py
@@ -1,25 +1,23 @@
 import os
-from openai import OpenAI
+from langchain_ollama import ChatOllama
+from langchain_core.messages import HumanMessage, SystemMessage
 from dotenv import load_dotenv
 import json
 
 load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_KEY"))
+client = ChatOllama(model=os.getenv("OLLAMA_MODEL_NAME"), temperature=0, num_predict=20)
 
 def llm_word_level(word: str, translation: str) -> str:
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": "Ты определяешь уровень CEFR слова."},
-            {"role": "user", "content": f"Слово: {word}\nПеревод: {translation}\nОтвечай строго JSON: {{'level':'A1/A2/B1/B2/C1/C2'}}"}
-        ],
-        max_completion_tokens=20
-    )
+    response = client.invoke([
+        SystemMessage(content=[f"Ты определяешь уровень CEFR слова. \nОтвечай строго JSON: {{'level':'A1/A2/B1/B2/C1/C2'}}",
+                      "Ответ должен быть подан без будь каких символов, кроме - {'level': 'A1/A2/B1/B2/C1/C2'}"]),
+        HumanMessage(content=f"Слово: {word}\nПеревод: {translation}")
+    ])
 
-    text = response.choices[0].message.content.strip()
+    text = response.content
+
     try:
         data = json.loads(text.replace("'", '"'))
         return data.get("level", "Unknown")
     except json.JSONDecodeError:
         return "Unknown"
-
