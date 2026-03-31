588e657fc501b1e3079c35e1c692e2210fb157b2
unknown <workboar@gmail.com>
2026-03-31 19:44:43 +0300

    Полностью рабочая написаная ллм для исправление ошибок, написан запрос в апи для этой модели и так же перенастроен докер

--- models/llm_correct_paragraph.py
+++ models/llm_correct_paragraph.py
@@ -2,6 +2,8 @@ import os
 from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, SystemMessage
 from dotenv import load_dotenv
+import difflib
+import re
 
 load_dotenv()
 client = ChatOllama(model=os.getenv("OLLAMA_MODEL_NAME"), temperature=0)
@@ -21,5 +23,41 @@ def correct_paragraph(user_sentence):
     ])
     return response.content
 
-# if __name__ == "__main__":
-#     print(correct_paragraph("Я ел суп завтра"))
+def tokenize(text):
+    return re.findall(r"\w+|[^\w\s]", text)
+
+def get_changed_word(user_sentence, corrected_sentence):
+    correct_changes = []
+    incorrect_changes = []
+
+    user_words = tokenize(user_sentence)
+    corrected_words = tokenize(corrected_sentence)
+
+    diff = difflib.ndiff(user_words, corrected_words)
+
+    for d in diff:
+        token = d[2:]
+
+        if d.startswith("+ "):
+            correct_changes.append(token)
+        elif d.startswith("- "):
+            # if re.fullmatch(r"[^\w\s]", token):
+            incorrect_changes.append(token)
+
+    return correct_changes, incorrect_changes
+
+
+
+if __name__ == "__main__":
+    user_sentence = "Я! завтра сьел кашу"
+    correct_sentence = correct_paragraph(user_sentence)
+
+    correct_words, incorrect_words = get_changed_word(user_sentence, correct_sentence)
+
+    ai_checking = {
+        "correction": correct_sentence,
+        "corrected_word": correct_words,
+        "incorrected_word": incorrect_words
+    }
+
+    print(ai_checking)
