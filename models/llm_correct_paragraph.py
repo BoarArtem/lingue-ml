9fc0462f797f0109e80b209ceb074aeec841aa92
unknown <workboar@gmail.com>
2026-03-31 11:01:33 +0300

    Интегрирована mistral-модель для задачи с исправлением ошибки в предложении

---
+++ models/llm_correct_paragraph.py
@@ -0,0 +1,25 @@
+import os
+from langchain_ollama import ChatOllama
+from langchain_core.messages import HumanMessage, SystemMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+client = ChatOllama(model=os.getenv("OLLAMA_MODEL_NAME_QWEN"), temperature=0)
+
+def correct_paragraph(user_sentence):
+    response = client.invoke([
+        SystemMessage(content=(
+            "You are a grammar corrector.",
+            "Correct any grammatical errors and adjust verb tenses according to context.",
+            "Do not change the meaning or words unnecessarily.",
+            "Do not translate.",
+            "Do not add explanations, parentheses, or extra text.",
+            "Always respond in the same language as the input.",
+            "Return ONLY the corrected sentence, nothing else."
+        )),
+        HumanMessage(content=user_sentence),
+    ])
+    return response.content
+
+# if __name__ == "__main__":
+#     print(correct_paragraph("Я ел суп завтра"))
