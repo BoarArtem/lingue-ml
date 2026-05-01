7b01be24a3cede645e67492bd485ee0208970596
Ruslan Podolian <120751851+RuslanPodolian@users.noreply.github.com>
2026-05-01 16:02:24 +0300

    Added inference minimal functionality for model

--- inference/anti_plagiarism_model_inference.py
+++ inference/anti_plagiarism_model_inference.py
@@ -4,8 +4,8 @@ from data.ai_or_human.dataset_executor import tokenizer, encode_text, tokenize_t
 from data.ai_or_human.dataset_executor import LABEL_MAP
 
 class AntiPlagiarismModelInference:
-    def __init__(self, vocab_size=tokenizer.vocab_size, embed_dim=128, hidden_dim=256, output_dim=3):
-        self.model = load_anti_plagiarism_model("anti_plagiarism_model.pth", vocab_size, embed_dim, hidden_dim, output_dim)
+    def __init__(self, model_path, vocab_size=tokenizer.vocab_size, embed_dim=128, hidden_dim=256, output_dim=3):
+        self.model = load_anti_plagiarism_model(model_path, vocab_size, embed_dim, hidden_dim, output_dim)
         self.model.eval()
 
     def get_label(self, text):
@@ -17,3 +17,9 @@ class AntiPlagiarismModelInference:
             output = self.model(input_tensor)
             _, predicted_label = torch.max(output, dim=-1)
             return LABEL_MAP[predicted_label.item()]
+
+if __name__ == "__main__":
+    inference_model = AntiPlagiarismModelInference("../models/anti_plagiarism/anti_plagiarism_model.pkl")
+    text = "This is a sample text to test the anti-plagiarism model."
+    label = inference_model.get_label(text)
+    print(f"Predicted label for the text: {label}")
