4a6cb31f880d65e57475d7a0c6d58e0b9f520aa6
Ruslan Podolian <120751851+RuslanPodolian@users.noreply.github.com>
2026-05-16 09:56:48 +0300

    Removed some unused imports

--- models/tacotron2.py
+++ models/tacotron2.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-# from data.ljspeech import LJSpeechDataset, get_dataloader
+from data.tts import LJSpeechDataset, get_dataloader
 from utils.general_training import apply_callbacks
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
