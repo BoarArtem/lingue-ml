51f82dfeef265e40019c5c7e9f9ebcc4e3ceeb77
BoarArtem <workboar@gmail.com>
2026-06-15 23:14:46 +0300

    implementing FasterWhisper STT-model inference

--- models/voice/stt.py
+++ models/voice/stt.py
@@ -1,12 +1,26 @@
 import whisper
+from faster_whisper import WhisperModel
 import torch
 
-class WhisperModel():
+class Whisper():
     def __init__(self):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.whisper_model = whisper.load_model(name="medium", device=self.device)
 
-    def inference(self, wav_pth: str):
+    def get_text(self, wav_pth: str):
         result = self.whisper_model.transcribe(audio=wav_pth, language="en")
 
         return result["text"]
+
+
+class FasterWhisper():
+    def __init__(self):
+        self.faster_whisper_model = WhisperModel("medium", device="cuda", compute_type="float16")
+
+    def get_text(self, wav_pth: str):
+        sigments, info = self.faster_whisper_model.transcribe(wav_pth, language="ru")
+
+        for sigment in sigments:
+            return sigment.text
+
+
